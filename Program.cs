10ef01cbc8215ea16b13db57ec487cbf7fc4fa43
jgerbs <jgerber4@my.bcit.ca>
2025-02-13 14:12:42 -0800

    Updated lab06

--- Program.cs
+++ Program.cs
@@ -2,19 +2,27 @@ using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using StudentFunctions.Models.School;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
+// Load environment variables and configuration
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+string connectionString = configuration["DATABASE_CONNECTION_STRING"];
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("DATABASE_CONNECTION_STRING is not set in the environment variables.");
+}
+
 // Add the DbContext and configure the connection string
 builder.Services.AddDbContext<SchoolContext>(options =>
-    options.UseSqlServer(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING")));
+    options.UseSqlServer(connectionString));
 
 builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
